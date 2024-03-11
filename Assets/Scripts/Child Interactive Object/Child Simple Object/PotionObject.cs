525f791829fcfec1686964d83d28a02853e9dcdd
Doerys <148343046+Aquelvin@users.noreply.github.com>
2024-03-11 17:25:22 +0100

    ajout classe

--- Assets/Scripts/Child Interactive Object/Child Simple Object/PotionObject.cs
+++ Assets/Scripts/Child Interactive Object/Child Simple Object/PotionObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class OneshotObject : SimpleObject
+public class PotionObject : SimpleObject
 {
     private Transform playerScale;
 
